a8f50e3ae97d521beda19033fd30bedac428e5a3
Hannah Saud Abdalla <hannah.abdalla@smartdubai.ae>
2019-09-08 14:49:10 +0400

    Initital commit

---
+++ BarcodeScannerApp/BarcodeScannerVC.swift
@@ -0,0 +1,110 @@
+//
+//  BarcodeScannerVC.swift
+//  BarcodeScannerApp
+//
+//  Created by Hannah Abdalla on 9/8/19.
+//  Copyright © 2019 Hannah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class  BarcodeScannerVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var barcodeValue: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            //            failed()
+            print("Failed")
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        } else {
+            //            failed()
+            print("Failed")
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+        dismiss(animated: true)
+    }
+    
+    func found(code: String) {
+        barcodeValue = code
+        print(code)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
--- BarcodeScannerApp/ViewController.swift
+++ BarcodeScannerApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barcodeField: UITextField!
+    var barcodeScanner: BarcodeScannerVC!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        barcodeScanner = BarcodeScannerVC()
     }
-
-
+    
+    @IBAction func scanBarcode(_ sender: Any) {
+        
+        launchBarcodeScannerVC()
+    }
+    
+    func launchBarcodeScannerVC() {
+        
+        present(barcodeScanner, animated: true, completion: nil)
+        while navigationController?.topViewController != self {}
+        barcodeField.text = barcodeScanner.barcodeValue
+    }
 }
 
