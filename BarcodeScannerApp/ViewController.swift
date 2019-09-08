0da0a4bef615fba769ecec00f3010c3aa434f178
Hannah Saud Abdalla <hannah.abdalla@smartdubai.ae>
2019-09-08 11:48:15 +0400

    Initial Commit

---
+++ BarcodeScannerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeScannerApp
+//
+//  Created by Hannah Abdalla on 9/8/19.
+//  Copyright © 2019 Hannah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
